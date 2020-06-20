bd57592e134fbd514ecab588966665140ab8af44
Sascha Kiefer <mail@sascha-kiefer.de>
2020-06-20 10:18:32 +0200

    Add Poetry

--- zshrc.sh
+++ zshrc.sh
@@ -5,7 +5,7 @@
 export ZSH=~/.oh-my-zsh
 
 # My path extensions
-export PATH=$PATH:~/Library/Python/3.8/bin:/usr/local/opt/python@3.8/bin
+export PATH=$PATH:~/Library/Python/3.8/bin:/usr/local/opt/python@3.8/bin:$HOME/.poetry/bin
 
 # Aliases
 alias mkdir="mkdir -vp"
