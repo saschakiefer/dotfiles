82bd494d31cd27a2569dc9a67cd040f545ebf4a8
Sascha Kiefer <mail@sascha-kiefer.de>
2021-05-26 11:42:27 +0200

    Conda and Ruby

--- zshrc.sh
+++ zshrc.sh
@@ -207,3 +207,23 @@ fi
 eval "$(pyenv init -)"
 
 export KUBECONFIG=$HOME/.kube/config
+export PATH="/usr/local/opt/openjdk/bin:$PATH"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/d044813/opt/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/d044813/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/d044813/opt/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/d044813/opt/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
+chruby ruby-2.7.2
