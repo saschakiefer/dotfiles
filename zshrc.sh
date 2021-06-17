007b8e44f907f7b4bf755015822e21a6739037e4
Sascha Kiefer <mail@sascha-kiefer.de>
2021-06-17 16:25:05 +0200

    Remove Java -> switch to sdkman.io

--- zshrc.sh
+++ zshrc.sh
@@ -231,14 +231,18 @@ eval "$(pyenv init -)"
 
 export KUBECONFIG=$HOME/.kube/config
 
-# Java
-export JAVA_16_HOME=$(/usr/libexec/java_home -v16)
-export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
+# # Java
+# export JAVA_16_HOME=$(/usr/libexec/java_home -v16)
+# export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
 
-alias java16='export JAVA_HOME=$JAVA_16_HOME'
-alias java11='export JAVA_HOME=$JAVA_11_HOME'
+# alias java16='export JAVA_HOME=$JAVA_16_HOME'
+# alias java11='export JAVA_HOME=$JAVA_11_HOME'
 
-# default to Java 11
-java11
+# # default to Java 11
+# java11
 
-export PATH="$JAVA_HOME/bin:$PATH"
+# export PATH="$JAVA_HOME/bin:$PATH"
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/Users/d044813/.sdkman"
+[[ -s "/Users/d044813/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/d044813/.sdkman/bin/sdkman-init.sh"
